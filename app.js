74aad08512247cdb93941a34a767fbc57d1d34e5
sanad <mohdsanadzakirizvi@gmail.com>
2016-11-22 12:08:53 +0530

    added sort/unsort feature

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 angular.module('photoApp', [])
-    .controller('PhotoAppController', ['$http', '$scope', function($http, $scope) {
+    .controller('PhotoAppController', ['$http', '$scope', 'orderByFilter',function($http, $scope, $orderBy) {
+
+        /*
+            1. Initialize images array 
+            2. initialize sortFactor
+        */
 
         $scope.images = [];
+        $scope.sortFactor = null;
 
         $scope.start = function() {
-            $scope.clicked = !$scope.clicked;
+            /*
+                1. Toggle fetchPhotosBtnClicked
+                2. send an http request to fetch the image data
+                3. iterate through the response and 'filter'(using Regex) images whose urls are broken 
+                4. push the newly formed image data to the images array
+            */
+            $scope.fetchPhotosBtnClicked = !$scope.fetchPhotosBtnClicked;
+
             $http.get('https://www.reddit.com/r/pics/.json?jsonp=').then(function(res) {
+                
                 angular.forEach(res.data.data.children, function(item) {
                     var patt = /jpg|png$/;
                     $scope.images.push({
@@ -14,9 +28,22 @@ angular.module('photoApp', [])
                         author: item.data.author
                     });
                 });
+
             }, function(err) {
                 console.log(err);
             });
+
+        };
+
+        $scope.sortBy = function(type){
+            /*
+                1. Toggle sortBtnClicked
+                2. Update the sortFactor 
+                3. Refresh the array order by calling $orderBy on array
+            */
+            $scope.sortBtnClicked = !$scope.sortBtnClicked;
+            $scope.sortFactor = type;
+            $scope.images = $orderBy($scope.images, $scope.sortFactor);
         };
 
     }]);
